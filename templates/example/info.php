cc172ee9c947f33ab56285d3847f3ceb22110650
Immanuel Bendahan <superimmi@yahoo.nl>
2019-04-05 19:20:34 +0200

    V4 comming up: Templates!

---
+++ templates/example/info.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Example template";
+$require_html = false;
+$author = "TheWebZone.nl";
+$suggested_title = ""; // $suggested_title == false --> suggested title gets ignored.
+$required_variables = [
+    //"variable name"=>"Text shown to user",
+    "var0"=>"Full name",
+    "var1"=>"Date of receive",
+    "var2"=>"E-mail was about",
+    "var3"=>"Receiver will be contacted through",
+    "var4"=>"When? On date or as soon as possible (fill in 'on [date]' or 'as soon as possible')"
+];
+?>
